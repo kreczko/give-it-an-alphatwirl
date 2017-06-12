b932942d9595e44bef816375ef2b2c03511e9655
kreczko <lkreczko@googlemail.com>
2017-06-12 14:48:03 +0100

    fixing setup to specific LCG view release

--- beginners/02_ntuple2categories/setup.sh
+++ beginners/02_ntuple2categories/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Using ROOT, GEANT, pandas etc from CVMFS"
-source /cvmfs/sft.cern.ch/lcg/views/LCG_latest/x86_64-slc6-gcc49-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_85/x86_64-slc6-gcc49-opt/setup.sh
 
 pip install --user -U -r requirements.txt
 
